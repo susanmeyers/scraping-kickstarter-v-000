650c13ee43c72a99e46a12ff5a026950d1abcb9c
Susan Meyers <smmeyers24@gmail.com>
2018-10-25 18:43:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ end
 create_project_hash
 
 # projects: 
-#kickstarter.css("li.project.grid_4")
-#project.css("h2.bbcard_name strong a").text
-#image link: project.css("div.project-thumbnail a img").attribute("src").value
+# kickstarter.css("li.project.grid_4")
+# project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
